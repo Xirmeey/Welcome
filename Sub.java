84968ef08908404cd25fb8fe603bbc87361ea886
OHIS <sirmueliruedo@gmail.com>
2019-09-04 16:44:08 +0100

    sub file

---
+++ Sub.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+   public class Subtraction{
+
+  public static void main(String[] arg){
+
+   Scanner input = new Scanner(System.in);
+
+  System.out.print("Please input first number:");
+   int number1 = input.nextInt();
+
+  System.out.print("Please input second number:");
+    int number2 = input.nextInt();
+
+   int Sub = number1 - number2 ;
+
+   System.out.printf("Sub %d%n", Sub);
+ 
+   }
+}
+
+
+// This is my new Subtraction file
